049167c3ce1585970316822582c2438bb25e03a9
Jeff Jagoda <jjagoda@bandwidth.com>
2014-09-28 13:39:44 -0400

    Configure bower.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,44 @@
 "use strict";
-var fs      = require("fs");
-var gulp    = require("gulp");
-var jscs    = require("gulp-jscs");
-var jshint  = require("gulp-jshint");
-var lab     = require("gulp-lab");
-var path    = require("path");
+var Bower   = require("gulp-bower");
+var FS      = require("fs");
+var Gulp    = require("gulp");
+var Jscs    = require("gulp-jscs");
+var Jshint  = require("gulp-jshint");
+var Lab     = require("gulp-lab");
+var Path    = require("path");
 var Q       = require("q");
-var stylish = require("jshint-stylish");
+var Stylish = require("jshint-stylish");
 var _       = require("lodash");
 
 var paths = {
-  jscs : path.join(__dirname, ".jscsrc"),
+  jscs : Path.join(__dirname, ".jscsrc"),
 
   jshint : {
-    source : path.join(__dirname, ".jshintrc"),
-    test   : path.join(__dirname, "test", ".jshintrc")
+    source : Path.join(__dirname, ".jshintrc"),
+    test   : Path.join(__dirname, "test", ".jshintrc")
   },
 
   source : [
-    path.join(__dirname, "*.js"),
-    path.join(__dirname, "lib", "**", "*.js")
+    Path.join(__dirname, "*.js"),
+    Path.join(__dirname, "lib", "**", "*.js"),
+    "!" + Path.join(__dirname, "lib", "static", "**", "*.js")
   ],
 
   test : [
-    path.join(__dirname, "test", "helpers", "setup.js"),
-    path.join(__dirname, "test", "**", "*_spec.js")
+    Path.join(__dirname, "test", "helpers", "setup.js"),
+    Path.join(__dirname, "test", "**", "*_spec.js")
   ]
 };
 
 function lint (options, files) {
-  return gulp.src(files)
-  .pipe(jshint(options))
-  .pipe(jshint.reporter(stylish))
-  .pipe(jshint.reporter("fail"));
+  return Gulp.src(files)
+  .pipe(new Jshint(options))
+  .pipe(Jshint.reporter(Stylish))
+  .pipe(Jshint.reporter("fail"));
 }
 
 function loadOptions (path) {
-  return Q.ninvoke(fs, "readFile", path, { encoding : "utf8" })
+  return Q.ninvoke(FS, "readFile", path, { encoding : "utf8" })
   .then(function (contents) {
     return JSON.parse(contents);
   });
@@ -52,30 +54,36 @@ function promisefy (stream) {
 }
 
 function style (options, files) {
-  return gulp.src(files).pipe(jscs(options));
+  return Gulp.src(files).pipe(new Jscs(options));
 }
 
-gulp.task("coverage", function () {
+Gulp.task("assets", [ "bower" ]);
+
+Gulp.task("bower", function () {
+  return (new Bower()).pipe(Gulp.dest("lib/static"));
+});
+
+Gulp.task("coverage", function () {
   var options = {
-    args : "-p -r html -o" + path.join(__dirname, "coverage.html"),
+    args : "-p -r html -o" + Path.join(__dirname, "coverage.html"),
     opts : { emitLabError : false }
   };
 
-  return gulp.src(paths.test).pipe(lab(options));
+  return Gulp.src(paths.test).pipe(new Lab(options));
 });
 
-gulp.task("default", [ "test" ]);
+Gulp.task("default", [ "test" ]);
 
-gulp.task("lint", [ "lint-source", "lint-test" ]);
+Gulp.task("lint", [ "lint-source", "lint-test" ]);
 
-gulp.task("lint-source", function () {
+Gulp.task("lint-source", function () {
   return loadOptions(paths.jshint.source)
   .then(function (options) {
     return promisefy(lint(options, paths.source));
   });
 });
 
-gulp.task("lint-test", function () {
+Gulp.task("lint-test", function () {
   return Q.all([
     loadOptions(paths.jshint.source),
     loadOptions(paths.jshint.test)
@@ -86,18 +94,18 @@ gulp.task("lint-test", function () {
   });
 });
 
-gulp.task("style", function () {
+Gulp.task("style", function () {
   return loadOptions(paths.jscs)
   .then(function (options) {
     return promisefy(style(options, paths.source.concat(paths.test)));
   });
 });
 
-gulp.task("test", [ "lint", "style" ], function () {
+Gulp.task("test", [ "assets", "lint", "style" ], function () {
   var options = {
     args : "-v -p -t 100",
     opts : { emitLabError : true }
   };
 
-  return gulp.src(paths.test).pipe(lab(options));
+  return Gulp.src(paths.test).pipe(new Lab(options));
 });
